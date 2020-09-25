8cd9b3c99c34bc1cf5b56f580a1f7f0a69f822fe
Jack <cph-jj484@cphbusiness.dk>
2020-09-25 10:56:12 +0200

    Added infrastructue package with database class resources package with database.sql file

---
+++ src/main/java/chadchat/infrastructure/database.java
@@ -0,0 +1,13 @@
+package chadchat.infrastructure;
+
+import chadchat.domain.User;
+
+import java.util.List;
+
+public class database {
+    private final String databaseLabel = "[SERVER]";
+    private final List<User> users;
+    private final List<String> messages;
+    private final List<String> channels;
+
+}
